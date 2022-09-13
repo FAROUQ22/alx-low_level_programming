fa82625e7e147a8f09ec4a01669e03560877fa30
Your Name <farouqasm1@gmail.com>
2022-09-12 22:42:50 -0700

    g

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  *main-program entry point.
- *   *
- *    *Return:0 if no error, non-zero value if error.
- *     */
+ * <F11>main-program entry point.
+ * Return:0 if no error, non-zero value if error.
+ */
 int main(void)
 {
 		int n;
