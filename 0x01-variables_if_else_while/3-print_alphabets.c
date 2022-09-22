3abd6fb354d831b4b5cd48a9de6eefc227ae99eb
Your Name <farouqasm1@gmail.com>
2022-09-22 07:20:19 -0700

    hi

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,23 @@
-nclude <stdio.h>
+#include <stdio.h>
+
 /**
- * main - entry point
+ *main - entry point
+ *
  * Return: 0 after printing the function
  */
 int main(void)
 {
-	int ch;	
-for (ch = 97; ch <= 122; ch++)
-{
-putchar(ch);
-}
-for (ch = 65; ch <= 90; ch++)
-{
-putchar(ch);
-}
-putchar(10);
-return (0);
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+		putchar(10);
+
+		return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
-nclude <stdio.h>
+#include <stdio.h>
+
 /**
- * main-program entry point.
- * Return:0 if no error, non zero if error.
+ *main-program entry point.
+ *Return:0 if no error, non zero if error.
  */
 int main(void)
 {
 	char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-if (i == 'e' || i == 'q')
-continue;
-putchar(i);
-}
-putchar('\n');
-return (0);
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
